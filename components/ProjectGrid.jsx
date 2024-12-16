c5761c611d39ef1055b8463d9065bda70e870bef
Marios Sofokleous <marios.sofokleous@yandex.com>
2024-12-16 13:28:39 +0200

    Make ProjectGrid responsive.

--- components/ProjectGrid.jsx
+++ components/ProjectGrid.jsx
@@ -5,7 +5,7 @@ export default function ProjectGrid({ projects }) {
   const baseUrl = process.env.NEXT_PUBLIC_STRAPI_API_URL;
 
   return (
-    <div className="grid grid-cols-2 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
       {projects.map((entry) => {
         const imageUrl = `${baseUrl}${entry.featuredImage.url}`;
 
