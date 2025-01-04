83265e90b017b2d81b7da09b2e37ea548beee12f
Marios Sofokleous <marios.sofokleous@yandex.com>
2025-01-04 14:32:48 +0200

    Dynamically set the cache strategy based on the environment.

--- next/lib/utils.js
+++ next/lib/utils.js
@@ -9,9 +9,10 @@ export async function fetchData(endpoint, options = {}) {
   const baseUrl = process.env.STRAPI;
   const url = new URL(endpoint, baseUrl);
 
+  const cacheStrategy = process.env.NODE_ENV === 'production' ? 'force-cache' : 'no-store';
+
   try {
-    const res = await fetch(url.toString(), { cache: 'force-cache', ...options });
-    // const res = await fetch(url.toString(), { cache: 'no-store', ...options });
+    const res = await fetch(url.toString(), { cache: cacheStrategy, ...options });
 
     if (!res.ok) {
       throw new Error(`Failed to fetch data from ${endpoint}`);
